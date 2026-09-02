733729b0f958f99a953621c5cca0307dd877fbec
rvp <renatovilcahuaman@gmail.com>
2026-09-01 23:12:13 -0500

    feat(ui): add DraftRecipeContext for in-progress recipe editing

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,13 +2,16 @@ import { Stack } from 'expo-router/stack';
 import { ErrorBoundary } from '../src/ui/ErrorBoundary';
 import { RecipeProvider } from '../src/ui/context/RecipeContext';
 import { IngredientProvider } from '../src/ui/context/IngredientContext';
+import { DraftRecipeProvider } from '../src/ui/context/DraftRecipeContext';
 
 export default function RootLayout() {
   return (
     <ErrorBoundary>
       <RecipeProvider>
         <IngredientProvider>
-          <Stack />
+          <DraftRecipeProvider>
+            <Stack />
+          </DraftRecipeProvider>
         </IngredientProvider>
       </RecipeProvider>
     </ErrorBoundary>
